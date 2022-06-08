be426895873e991810239e3835c27d59674c2a04
Omar Jarkas <s4598179@labs.eait.uq.edu.au>
2022-06-08 18:29:20 +1000

    adding zlibwapi

--- custom_train_1920.sh
+++ custom_train_1920.sh
@@ -1,1 +1,3 @@
 python train.py --img 1920 --batch 16 --epochs 3 --data meat1000.yaml --weights yolov5s.pt
+
+python train.py --img 1920 --batch 16 --epochs 3 --data meat1000.yaml --weights yolov5s.pt 
